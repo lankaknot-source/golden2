ed82a181d883c39adb4ab0ce0e81ed43ef4cc015
lankaknot-source <lankaknot@gmail.com>
2026-09-23 12:13:07 +0530

    Fix iOS scene delegate startup

--- flutter_app/ios/Runner/SceneDelegate.swift
+++ flutter_app/ios/Runner/SceneDelegate.swift
@@ -1,6 +1,10 @@
 import Flutter
 import UIKit
 
+// The scene class is referenced by name from Info.plist. Explicitly exposing
+// the Objective-C name keeps it loadable in release archives as well as debug
+// builds (otherwise iOS can leave the app on a blank scene).
+@objc(SceneDelegate)
 class SceneDelegate: FlutterSceneDelegate {
 
 }
